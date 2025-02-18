1a86075e46c3f3f4d4e0a31a8eab404bacaa32a4
Huguie <Hyj2862588507@163.com>
2025-02-18 08:11:54 -0800

    update IB_CH_MD_LSX_2022 and IB_CH_KEF_CZS_2014

--- src/test/java/IBCHTest.java
+++ src/test/java/IBCHTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -31,9 +32,11 @@ public class IBCHTest {
                 scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.PublicParam pp = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.PublicParam();
                 scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.MasterSecrtKey msk = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.MasterSecrtKey();
                 scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Trapdoor td = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Trapdoor();
-                scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.HashValue h = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.HashValue();
-                scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness r = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness();
-                scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness r_p = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness();
+                scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.HashValue h1 = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.HashValue();
+                scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.HashValue h2 = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.HashValue();
+                scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness r1 = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness();
+                scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness r2 = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness();
+                scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness r1_p = new scheme.IBCH.IB_CH_KEF_CZS_2014.PBC.Randomness();
 
                 Element m,m_p;  // message
                 String ID = "identity string ID";
@@ -45,13 +48,17 @@ public class IBCHTest {
 
                 ch.Extract(td, ID, msk);
 
-                ch.Hash(h, r, ID, L, m, pp);
+                ch.Hash(h1, r1, ID, L, m, pp);
+                ch.Hash(h2, r2, ID, L, m_p, pp);
+                assertTrue(ch.Check(h1, r1, L, m, td), "H(m) valid");
+                assertTrue(ch.Check(h2, r2, L, m_p, td), "H(m_p) valid");
+                assertFalse(ch.Check(h1, r1, L, m_p, td), "not H(m)");
+                assertFalse(ch.Check(h2, r2, L, m, td), "not H(m_p)");
 
-                assertTrue(ch.Check(h, r, L, m, td), "Check failed");
+                ch.Adapt(r1_p, m_p, h1, r1, L, m, td);
 
-                ch.Adapt(r_p, m_p, h, r, L, m, td);
-
-                assertTrue(ch.Check(h, r_p, L, m_p, td), "Verify failed");
+                assertTrue(ch.Check(h1, r1_p, L, m_p, td), "adapted m_p valid");
+                assertFalse(ch.Check(h1, r1_p, L, m, td), "not adapted m");
             }
         }
     }
@@ -72,9 +79,11 @@ public class IBCHTest {
                 scheme.IBCH.IB_CH_MD_LSX_2022.PBC.PublicParam pp = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.PublicParam();
                 scheme.IBCH.IB_CH_MD_LSX_2022.PBC.MasterSecrtKey msk = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.MasterSecrtKey();
                 scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Trapdoor td = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Trapdoor();
-                scheme.IBCH.IB_CH_MD_LSX_2022.PBC.HashValue h = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.HashValue();
-                scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness r = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness();
-                scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness r_p = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness();
+                scheme.IBCH.IB_CH_MD_LSX_2022.PBC.HashValue h1 = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.HashValue();
+                scheme.IBCH.IB_CH_MD_LSX_2022.PBC.HashValue h2 = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.HashValue();
+                scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness r1 = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness();
+                scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness r2 = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness();
+                scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness r1_p = new scheme.IBCH.IB_CH_MD_LSX_2022.PBC.Randomness();
 
                 Element m,m_p;  // message
                 Element ID;
@@ -87,13 +96,16 @@ public class IBCHTest {
 
                 ch.KeyGen(td, ID, msk, pp);
 
-                ch.Hash(h, r, ID, m, pp);
+                ch.Hash(h1, r1, ID, m, pp);
+                ch.Hash(h2, r2, ID, m_p, pp);
+                assertTrue(ch.Check(h1, r1, ID, m, pp), "H(m) valid");
+                assertTrue(ch.Check(h2, r2, ID, m_p, pp), "H(m_p) valid");
+                assertFalse(ch.Check(h1, r1, ID, m_p, pp), "not H(m)");
+                assertFalse(ch.Check(h2, r2, ID, m, pp), "not H(m_p)");
 
-                assertTrue(ch.Check(h, r, ID, m, pp), "Check failed");
-
-                ch.Adapt(r_p, h, m, r, m_p, td);
-
-                assertTrue(ch.Check(h, r_p, ID, m_p, pp), "Verify failed");
+                ch.Adapt(r1_p, h1, m, r1, m_p, td);
+                assertTrue(ch.Check(h1, r1_p, ID, m_p, pp), "adapted m_p valid");
+                assertFalse(ch.Check(h1, r1_p, ID, m, pp), "not adapted m");
             }
         }
     }
